ecbe18b1debf8fa890f8ac953f22a395b7b84bd4
cutproof <samjsem@yahoo.com>
2019-09-10 15:22:46 -0400

    Initial Test Deployment Commit ...

--- src/main/java/com/ilinksolutions/p3m1/rservices/P3MIRestController.java
+++ src/main/java/com/ilinksolutions/p3m1/rservices/P3MIRestController.java
@@ -8,35 +8,64 @@ import org.slf4j.LoggerFactory;
 import com.ilinksolutions.p3m1.domains.UKVisaMessage;
 import com.ilinksolutions.p3m1.bservices.UKVisaService;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-public class P2RestController
+public class P3MIRestController
 {
-	Logger logger = LoggerFactory.getLogger(P2RestController.class);
+	Logger logger = LoggerFactory.getLogger(P3MIRestController.class);
 	
-    @GetMapping("/getmsg/{id}")
-	public ResponseEntity<UKVisaMessage> readEntry(@PathVariable String id)
-    {
-    	logger.info("P2RestController: readEntry: Begin!");
-    	logger.info("P2RestController: readEntry: Path Variable: " + id);
-        UKVisaService service = new UKVisaService();
-        UKVisaMessage returnValue = service.getEntry(new Integer(id).intValue());
+	@RequestMapping(value = "/fileMessage", method = RequestMethod.POST, headers = {"content-type=multipart/mixed","content-type=multipart/form-data"})
+	public ResponseEntity<UKVisaMessage> fileMessage(@RequestHeader HttpHeaders headers,
+	        @PathVariable String userId,
+	        @RequestPart(value = "image", required = false) MultipartFile image,
+	        @RequestPart(value = "", required = true) UKVisaMessage json)
+	{
+		logger.info("P3MIRestController: fileMessage: Begin!");
+		logger.info("P3MIRestController: fileMessage: JSON: " + json.toString());
+		logger.info("P3MIRestController: fileMessage: End.");
+		UKVisaService service = new UKVisaService();
+		UKVisaMessage returnValue = service.getEntry(new Integer(1).intValue());
         if (returnValue == null)
         {
-        	logger.info("P2RestController: readEntry: returnValue: NULL");
+        	logger.info("P3MIRestController: fileMessage: returnValue: NULL");
             return ResponseEntity.notFound().build();
         }
         else
         {
-            logger.info("P2RestController: readEntry: returnValue: " + returnValue.toString());
+            logger.info("P3MIRestController: fileMessage: returnValue: " + returnValue.toString());
+            return ResponseEntity.ok(returnValue);
+        }
+	}
+	
+    @GetMapping("/getmsg/{id}")
+	public ResponseEntity<UKVisaMessage> readEntry(@PathVariable String id)
+    {
+    	logger.info("P3MIRestController: readEntry: Begin!");
+    	logger.info("P3MIRestController: readEntry: Path Variable: " + id);
+        UKVisaService service = new UKVisaService();
+        UKVisaMessage returnValue = service.getEntry(new Integer(id).intValue());
+        if (returnValue == null)
+        {
+        	logger.info("P3MIRestController: readEntry: returnValue: NULL");
+            return ResponseEntity.notFound().build();
+        }
+        else
+        {
+            logger.info("P3MIRestController: readEntry: returnValue: " + returnValue.toString());
             return ResponseEntity.ok(returnValue);
         }
     }
@@ -44,18 +73,18 @@ public class P2RestController
     @PostMapping("/savemsg")
     public ResponseEntity<UKVisaMessage> registerMessage(@RequestBody UKVisaMessage message)
     {
-    	logger.info("registerMessage: registerMessage: Begin.");
-    	logger.info("registerMessage: registerMessage: Transform: " + message.toString());
+    	logger.info("P3MIRestController: registerMessage: Begin.");
+    	logger.info("P3MIRestController: registerMessage: Transform: " + message.toString());
     	UKVisaService service = new UKVisaService();
     	UKVisaMessage returnValue = service.addEntry(message);
     	if (returnValue == null)
     	{
-    		logger.info("registerMessage: registerMessage: id: NULL.");
+    		logger.info("P3MIRestController: registerMessage: id: NULL.");
             return ResponseEntity.notFound().build();
         }
     	else
     	{
-    		logger.info("registerMessage: registerMessage: id: End.");
+    		logger.info("P3MIRestController: registerMessage: id: End.");
             URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(returnValue.getId()).toUri();
             return ResponseEntity.created(uri).body(returnValue);
         }
@@ -64,6 +93,7 @@ public class P2RestController
     @PutMapping("/updatemsg/{id}")
     public ResponseEntity<UKVisaMessage> update(@RequestBody UKVisaMessage message, @PathVariable int id)
     {
+    	logger.info("P3MIRestController: update: Begin.");
         UKVisaService service = new UKVisaService();
         UKVisaMessage returnValue = service.updateEntry(id, message);
         if (returnValue == null)
